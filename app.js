41ce54dce36bf7a6208af5b7fcc3c9b2fe2aaaf7
muhammadbilalahmed2 <muhammadbilalahmed434@gmail.com>
2024-08-22 01:48:07 +0500

    22-8-24

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log('hello world');
+
+alert()
+prompt()
+
+
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+
+
+
+
+
+
+
+
+
+
+function greetUser (){   
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+}
