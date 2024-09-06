98051f7e5db8d3c5e903a2fd6c0d812a05bba78c
muhammadbilalahmed2 <muhammadbilalahmed434@gmail.com>
2024-09-07 03:19:11 +0500

    practice

--- app.js
+++ app.js
@@ -25,3 +25,41 @@ console.log('hello world');
 console.log('hello world');
 console.log('hello world');
 }
+
+
+
+// Assignment Operator
+// =
+// +=
+// -=
+// /=
+// *=
+
+var num1 = 12;
+num1 = num1 + 10;
+num1 += 10;
+num1 -= 10;
+num1 *= 10;
+num1 /= 10;
+console.log(num1);
+
+
+
+// Comparision Operator
+
+// >
+// <
+// == || !=
+// === || !==
+// != data type check nahi kraiga!
+// !== data type check kraiga!
+// >=
+// <=
+
+console.log(10 == '10');
+console.log(10 === '10');
+
+console.log(10 != 10);
+console.log(10 == '10');
+var username = 'abdullah'
+document.write('<h1>'+ username+ '</h1>');
