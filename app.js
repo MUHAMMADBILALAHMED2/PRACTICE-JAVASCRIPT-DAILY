d4b118aa9a1dcded9d0fc8121d4d04495bf92f1a
muhammadbilalahmed2 <muhammadbilalahmed434@gmail.com>
2024-09-07 03:23:21 +0500

    practice

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-console.log('hello world');
+// console.log('hello world');
 
-alert()
-prompt()
-
-
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
+// alert()
+// prompt()
 
 
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
 
 
 
@@ -19,12 +17,14 @@ console.log('hello world');
 
 
 
-function greetUser (){   
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-}
+
+
+// function greetUser (){   
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// }
 
 
 
@@ -35,13 +35,13 @@ console.log('hello world');
 // /=
 // *=
 
-var num1 = 12;
-num1 = num1 + 10;
-num1 += 10;
-num1 -= 10;
-num1 *= 10;
-num1 /= 10;
-console.log(num1);
+// var num1 = 12;
+// num1 = num1 + 10;
+// num1 += 10;
+// num1 -= 10;
+// num1 *= 10;
+// num1 /= 10;
+// console.log(num1);
 
 
 
@@ -56,10 +56,52 @@ console.log(num1);
 // >=
 // <=
 
-console.log(10 == '10');
-console.log(10 === '10');
+// console.log(10 == '10');
+// console.log(10 === '10');
 
-console.log(10 != 10);
-console.log(10 == '10');
-var username = 'abdullah'
-document.write('<h1>'+ username+ '</h1>');
+// console.log(10 != 10);
+// console.log(10 == '10');
+// var username = 'abdullah'
+// document.write('<h1>'+ username+ '</h1>');
+
+
+// num++ post increment
+// ++num pre increment 
+// var num = 10;
+// console.log(num + 1);
+// console.log(++num);
+// console.log(num++);
+// console.log(num++)
+// console.log(++num)
+
+
+
+
+
+// var num = 10
+// var calculation = ++num + num++ + ++num;
+// 11 + 11 + 13
+// var calculation = ++num + num++ + ++num + --num + num-- + num++ + ++num;
+// //11 + 11 + 13 + 12 + 12 + 11 + 13;
+
+
+// console.log(calculation , 11 + 11 + 13 + 12 + 12 + 11 + 13);
+
+
+
+
+
+
+
+
+
+
+
+
+
+var num = 2;
+if(num % 2 === 0){
+    console.log('number is divisible by 2');
+}else{
+    console.log('number is not divisible by 2');
+}
